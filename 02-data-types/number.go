39173f8b638c2c9294003965cac9ddfd5c7e2a11
Moo Yeol, Lee (Prescott) <mooyoul@gmail.com>
2016-04-10 01:25:10 +0900

    Added Chapter 02 - Data Types - Number

---
+++ 02-data-types/number.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(" Integer ===>")
+	fmt.Println(" 1 + 1 = ", 1+1)
+	fmt.Println(" 3 * 3 = ", 3*3)
+	fmt.Println(" 10 - 3 = ", 10-3)
+	fmt.Println(" 11 % 4 = ", 11%4)
+
+	fmt.Println("Flating point number ===>")
+	fmt.Println(" 1.3 * 3 = ", 1.3*3)
+	fmt.Println(" 3 / 2 = ", 3/2)
+	fmt.Println(" 1.01 - 0.99 = ", 1.01-0.99)
+}
